2548474d6c29d55f5202d7c19b8bdf1c65ca5e5a
Manny Aguilera <maguilera5@ewu.edu>
2023-12-31 23:32:56 +0000

    file name change

--- arctic-80s/src/Home.jsx
+++ arctic-80s/src/Home.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import './App.css'
+import './Home.css'
 
 function Home() {
 
